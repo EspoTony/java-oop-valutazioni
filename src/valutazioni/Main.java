f010bbd8b44b5c18e1e7b4af4a58ab8178dcfa1e
EspoTony <espotony98@gmail.com>
2022-06-16 14:09:51 +0200

    fine esercizio

---
+++ src/valutazioni/Main.java
@@ -0,0 +1,25 @@
+package valutazioni;
+
+import java.util.Random;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		Valutazioni[] arrayValutazioni = new Valutazioni[20];
+		Random r = new Random();
+		for (int i=0; i<arrayValutazioni.length; i++) {
+			int assenze = r.nextInt(100);
+			float mediaVoti = (r.nextFloat() * 5);
+			arrayValutazioni[i] = new Valutazioni(i+1, assenze, mediaVoti);
+			
+			System.out.printf("Id: %2d Valutazione: ", (i+1) );
+			
+			if (arrayValutazioni[i].Valutazione()) {
+				System.out.println("Promosso");
+			}else {
+				System.out.println("Bocciato");
+			}
+		}
+	}
+
+}
---
+++ src/valutazioni/Valutazioni.java
@@ -0,0 +1,23 @@
+package valutazioni;
+
+public class Valutazioni {
+	int id,assenze;
+	float mediaVoti;
+	
+	Valutazioni(int id, int assenze, float mediaVoti) {
+	this.id = id;
+	this.assenze = assenze;
+	this.mediaVoti = mediaVoti;
+	}
+	
+	boolean Valutazione () {
+		boolean promosso = false;
+		
+		if (assenze < 25 && mediaVoti>= 2) {
+			promosso=true;
+		} else if (assenze <= 50 && mediaVoti > 2) {
+			promosso=true;
+		}
+		return promosso;
+	}
+}
