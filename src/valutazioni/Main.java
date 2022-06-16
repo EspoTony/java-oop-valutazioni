7609b729b816f021a2de6d594ee478f4592f29cf
EspoTony <espotony98@gmail.com>
2022-06-16 15:13:53 +0200

    esercizio finito con totalte studenti promossi

--- src/valutazioni/Main.java
+++ src/valutazioni/Main.java
@@ -5,8 +5,11 @@ import java.util.Random;
 public class Main {
 	
 	public static void main (String[] args) {
+		
 		Valutazioni[] arrayValutazioni = new Valutazioni[20];
+		
 		Random r = new Random();
+		
 		for (int i=0; i<arrayValutazioni.length; i++) {
 			int assenze = r.nextInt(100);
 			float mediaVoti = (r.nextFloat() * 5);
@@ -14,11 +17,15 @@ public class Main {
 			
 			System.out.printf("Id: %2d Valutazione: ", (i+1) );
 			
+			int studProm=0;
+			
 			if (arrayValutazioni[i].Valutazione()) {
 				System.out.println("Promosso");
+				studProm++;
 			}else {
 				System.out.println("Bocciato");
 			}
+			System.out.println("in totale sono stati promossi " +studProm+" studenti");
 		}
 	}
 
